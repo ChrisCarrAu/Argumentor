02e9ed9495747e0e1a6bc988995a7636368ac0be
Chris Carr <chris@appthem.com>
2018-02-11 09:12:34 +1100

    Add project files.

---
+++ ArgumentHandler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArgumentHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
